ef45c62c46159e93dfae6025ac832a9c4e01b3ae
David Daniel <davedan7@gmail.com>
2015-09-23 20:39:59 -0600

    Basics all working

--- test/convert-test.js
+++ test/convert-test.js
@@ -23,16 +23,34 @@ describe('num', function() {
     assert.equal(n3, "nine and 92/100 dollars");
   });
 
-  xit('converts teens', function () {
+  it('converts teens', function () {
     var n1 = convert(10);
     var n2 = convert(11);
-    var n3 = convert(9);
+    var n3 = convert(13);
 
     assert.equal(n1, "ten and 00/100 dollars");
     assert.equal(n2, "eleven and 00/100 dollars");
     assert.equal(n3, "thirteen and 00/100 dollars");
   });
 
+  it('converts teens with cents', function () {
+    var n1 = convert(10.30);
+    var n2 = convert(17.77);
+    var n3 = convert(19.50);
 
+    assert.equal(n1, "ten and 30/100 dollars");
+    assert.equal(n2, "seventeen and 77/100 dollars");
+    assert.equal(n3, "nineteen and 50/100 dollars");
+  });
+
+  it('converts large numbers regardless of decimal', function () {
+    var n1 = convert(1000000.00);
+    var n2 = convert(1000000);
+    var n3 = convert(3333333.33);
+
+    assert.equal(n1, "one million and 00/100 dollars");
+    assert.equal(n2, "one million and 00/100 dollars");
+    assert.equal(n3, "three million three hundred thirty-three thousand three hundred thirty-three and 33/100 dollars");
+  });
 
 });
